e55856c3747ac7561eb016c0e70dfa31abe7a104
Vahtee Boo <ronnie.kaa@bigmir.net>
2021-12-13 19:21:23 +0200

    Initial Commit

---
+++ CryptoCompareSwiftUI/CryptoCompareSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCompareSwiftUIApp.swift
+//  CryptoCompareSwiftUI
+//
+//  Created by Vahtee Boo on 13.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCompareSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
